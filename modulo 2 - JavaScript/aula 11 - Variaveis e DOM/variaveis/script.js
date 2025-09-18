8af9505406220eea4f2c23f41971e9de4a2d0122
Biel-prog <meu.negocio.gr@gmail.com>
2025-09-18 10:48:11 -0300

    INTRODUÇÃO AO JAVASCRIPT
    VARIAVEIS E DOM

---
+++ modulo 2 - JavaScript/aula 11 - Variaveis e DOM/variaveis/script.js
@@ -0,0 +1,18 @@
+console.log("String" + "Hello Word");
+console.log("Número " + (2 + 2));
+console.log("Booleano" + true);
+console.log("Hello Word");
+
+//VARIAVEIS
+
+// var, valor pode ser atribuido e alterado e tem escopo GLOBAL
+var nome = "Matheus"
+nome = "Robson"
+console.log(nome);
+
+//let, valor pode ser alterado tambem, mas tem escopo LOCAL
+let sobrenome = "Souza"
+sobrenome = "Silva"
+
+//Constante, valor não pode ser alterado
+const pi = 3.14

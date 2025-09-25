b640d9ef5aae64e6e8fcbf2c699f4b1efe84dd25
Celegasz <meu.negocio.gr@gmail.com>
2025-09-25 10:57:09 -0300

    .

---
+++ modulo 2 - JavaScript/aula 13/desafio/script.js
@@ -0,0 +1,6 @@
+function calcular(){
+const inputnum1 = document.getElementById("num1");
+const inputnum2 = document.getElementById("num2");
+const selecoperacao = document.getElementById("operacao")
+
+}
